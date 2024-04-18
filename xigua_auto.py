947b487eb70cf38bb4dd57ec3b84595979cac0cb
Tiger Ji <tiger@101healthcare.cn>
2024-04-18 20:06:05 +0800

    none

--- xigua_auto.py
+++ xigua_auto.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 import re
 
-def run(playwright: Playwright,video:dict,state_storage:str) -> None:
-    browser = playwright.chromium.launch(headless=True)
+def run_w(playwright: Playwright,video:dict,state_storage:str) -> None:
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state=state_storage)
     page = context.new_page()
     page.goto("https://studio.ixigua.com/upload")
@@ -11,9 +11,8 @@ def run(playwright: Playwright,video:dict,state_storage:str) -> None:
         page.get_by_text("点击上传或将文件拖入此区域").click()
     file_chooser = fc_info.value
     file_chooser.set_files(video.get("url"))
-    # page.locator("body").set_input_files("./data/kuaishou/3xc4x9nmqa5825y.mp4")
-    # page.pause()
-    # page.wait_for_timeout(5000)
+    page.pause()
+    page.wait_for_timeout(5000)
     page.locator("div").filter(has_text=re.compile(r"^上传成功$")).wait_for()
     # page.locator("div").filter(has_text=re.compile(r"^5-30个字符，标题含有关键词，可以被更多人看到0/30$")).get_by_role("combobox").locator("div").nth(2).click()
     page.locator("div").filter(has_text=re.compile(r"^5-30个字符，标题含有关键词，可以被更多人看到0/30$")).get_by_role("combobox").fill(video.get("name","什么都不是"))
@@ -41,12 +40,43 @@ def run(playwright: Playwright,video:dict,state_storage:str) -> None:
     context.close()
     browser.close()
 
+def run_h(playwright: Playwright,video:dict,state_storage:str) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state=state_storage)
+    page = context.new_page()
+    page.goto("https://studio.ixigua.com/upload")
+    with page.expect_file_chooser() as fc_info:
+        page.locator("path:nth-child(4)").first.click()
+    file_chooser = fc_info.value
+    file_chooser.set_files(video.get("url"))
+    page.pause()
+    page.wait_for_timeout(5000)
+    page.locator("div").filter(has_text=re.compile(r"^上传成功$")).wait_for()
+    page.get_by_role("combobox").locator("div").nth(2).click()
+    page.get_by_role("combobox").fill(video.get("name","什么都不是"))
+    page.get_by_text("上传封面").click()
+    page.get_by_text("本地上传").click()
+    with page.expect_file_chooser() as fc_info:
+        page.get_by_text("点击上传或将图片拖入此区域").click()
+    file_chooser = fc_info.value
+    file_chooser.set_files(video.get("cover_url"))
+    page.get_by_text("完成裁剪", exact=True).click()
+    page.get_by_role("button", name="确定").click()
+    page.pause()
+    page.locator("div").filter(has_text=re.compile(r"^取消确定$")).get_by_role("button", name="确定").click()
+    page.get_by_role("button", name="发布").click()
+    page.wait_for_timeout(5000)
+    page.close()
+
+    # ---------------------
+    context.close()
+    browser.close()
 
 with sync_playwright() as playwright:
     video = dict(
         name='什么都不是',
-        url='./data/kuaishou/3xc4x9nmqa5825y.mp4',
-        cover_url='',
+        url='./data/kuaishou/3xpeki6fwm6uvbc.mp4',
+        cover_url='./data/kuaishou/3xpeki6fwm6uvbc.jpeg',
         topic='网海钩陈'
     )
-    run(playwright,video,state_storage='./auth/state.json')
+    run_w(playwright,video,state_storage='./auth/state.json')
